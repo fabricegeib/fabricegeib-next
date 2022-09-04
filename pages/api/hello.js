075972194ced67d90c5cedd47fa4c3c29caf6214
Fabrice Geib <fabricegeib@gmail.com>
2022-09-04 19:07:03 +0200

    api updates

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    title: "hello api",
+    owner: "Fabrice Geib",
+    results: [
+      {
+        name: "Charly",
+      },
+      {
+        name: "Pierre",
+      },
+      {
+        name: "Sam",
+        films: [
+          "https://next-fabricegeib.vercel.app/api/films/1/",
+          "https://next-fabricegeib.vercel.app/api/films/2/",
+          "https://next-fabricegeib.vercel.app/api/films/3/",
+          "https://next-fabricegeib.vercel.app/api/films/6/",
+        ],
+      },
+    ],
+  });
 }
